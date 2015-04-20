321d2357c196f0993fa056bd5f22572ae3fa76b0
Jamie Magee <jamie.magee@gmail.com>
2015-04-20 15:42:37 +0200

    Add scripts

---
+++ scripts/flair.py
@@ -0,0 +1,138 @@
+import os
+import time
+import datetime
+import json
+from urllib import request
+from io import BytesIO
+from configparser import ConfigParser
+
+from PIL import Image
+from azure.storage import CloudStorageAccount
+import praw
+
+def get_flair_info(message):
+    info = json.loads(message.body)
+    return info.popitem()
+
+
+def assign_flair(r, message, text, position):
+    r.set_flair(subreddit, message.author, text, position)
+    message.mark_as_read()
+
+
+def get_css_images(r, subreddit):
+    stylesheet = r.get_stylesheet(subreddit)
+    css = stylesheet['stylesheet']
+    image = next(image['url'] for image in stylesheet['images'] if image['name'] == 'flairsheet')
+    return css, image
+
+
+def upload_image(r, image, subreddit):
+    r._request(r.config['upload_image'],
+               data={'r': subreddit, 'img_type': 'png', 'name': 'flairsheet'},
+               files={'file': image.getvalue()})
+
+
+def make_new_flairsheet(flairsheet, new_flair):
+    flairsheet = Image.open(BytesIO(request.urlopen(flairsheet).read()))
+    new_flair = Image.open(BytesIO(blob_service.get_blob_to_bytes('images', new_flair + '.png')))
+
+    new_flairsheet = Image.new("RGBA", [flairsheet.size[0], flairsheet.size[1] + new_flair.size[1]])
+    new_flairsheet.paste(flairsheet, (0, 0))
+    new_flairsheet.paste(new_flair, (0, flairsheet.size[1], flairsheet.size[0], flairsheet.size[1] + new_flair.size[1]))
+
+    flairsheet = BytesIO()
+    new_flairsheet.save(flairsheet, 'png')
+    position = new_flairsheet.size[1] // new_flair.size[1] - 1
+
+    return flairsheet, position, new_flair.size[1]
+
+
+def append_css(r, subreddit, css, position, height):
+    new_css = '.flair-' + str(position) + '{background-position: 0 -' + str(height * position) + 'px}'
+    r.set_stylesheet(subreddit, css + new_css)
+
+
+def log(message):
+    table_service.insert_entity('logs', {'PartitionKey': 'flair', 'RowKey': str(datetime.datetime.now()),
+                                         'text': message})
+    print('[*] ' + message)
+
+if os.path.isfile(os.path.join(os.path.dirname(__file__), 'settings.cfg')):
+	config = ConfigParser()
+	config.read(os.path.join(os.path.dirname(__file__), 'settings.cfg'))
+	
+	subreddit = config.get('reddit', 'subreddit')
+	username = config.get('reddit', 'username')
+	password = config.get('reddit', 'password')
+	
+	storage_account_name = config.get('azure', 'name')
+	storage_account_key = config.get('azure', 'key')
+else:
+    subreddit = os.getenv('SUBREDDIT')
+    username = os.getenv('USERNAME')
+    password = os.getenv('PASSWORD')
+
+    storage_account_name = os.getenv('STORAGE_ACCOUNT_NAME')
+    storage_account_key = os.getenv('STORAGE_ACCOUNT_KEY')
+
+storage_account = CloudStorageAccount(storage_account_name, storage_account_key)
+
+table_service = storage_account.create_table_service()
+blob_service = storage_account.create_blob_service()
+
+blob_service.create_container('images', x_ms_blob_public_access='container')
+table_service.create_table('flair')
+table_service.create_table('logs')
+
+r = praw.Reddit('RowingFlair by /u/Jammie1')
+r.login(username, password)
+r.config.decode_html_entities = True
+
+while True:
+    for message in (m for m in r.get_unread(limit=None) if m.subject == 'f'):
+        log('received mesage from ' + message.author.name)
+        try:
+            file, text = get_flair_info(message)
+            if blob_service.list_blobs('images', file + '.png'):
+                if table_service.query_entities('flair', "RowKey eq '" + file + "'"):
+                    flair = table_service.get_entity('flair', 'flair', file)
+                    assign_flair(r, message, text, flair.position)
+                    r.send_message(message.author, 'Rowing flair success',
+                                   'Your flair has been set')
+                    log('Assigned existing flair: ' + file + ' with text: ' + text + ' to user: ' + message.author.name)
+
+                else:
+                    css, flairsheet = get_css_images(r, subreddit)
+
+                    flairsheet, position, height = make_new_flairsheet(flairsheet, file)
+                    log('Generated new flairsheet')
+
+                    try:
+                        upload_image(r, flairsheet, subreddit)
+                        append_css(r, subreddit, css, position, height)
+                        table_service.insert_entity('flair',
+                                                    {'PartitionKey': 'flair', 'RowKey': file, 'position': position})
+
+                        assign_flair(r, message, text, position)
+                        r.send_message(message.author, 'Rowing flair success',
+                                       'Your flair has been set')
+                        log('Assigned flair: ' + file + ' with text: ' + text + ' to user: ' + message.author.name)
+                    except praw.errors.BadCSS:
+                        message.mark_as_read()
+                        r.send_message(message.author, 'Rowing flair error',
+                                       'Oops! Something went wrong.')
+                        r.send_message('jammie1', 'Rowing flair error',
+                                       'Hit CSS limit. Check logs.')
+            else:
+                message.mark_as_read()
+                r.send_message(message.author, 'Rowing flair error',
+                               'Sorry, I couldn\'t find a flair with that filename')
+                log('Couldn\'t find flair with name:' + file)
+
+        except Exception as e:
+            message.mark_as_read()
+            r.send_message(message.author, 'Rowing flair error', 'Sorry, I couldn\'t find a flair with that filename')
+            log('Error reading user message')
+
+    time.sleep(30)
---
+++ scripts/scraper.py
@@ -0,0 +1,136 @@
+import re
+from urllib import parse, request
+import os
+import datetime
+import time
+from io import BytesIO
+from configparser import ConfigParser
+
+from PIL import Image
+from azure.storage import CloudStorageAccount
+from bs4 import BeautifulSoup
+import praw
+
+def log(message):
+    table_service.insert_entity('logs',
+                                    {'PartitionKey': 'scraper', 'RowKey': str(datetime.datetime.now()), 'text': message})
+    print('[*] ' + message)
+
+def download_images(url, level):
+    global website
+    netloc = parse.urlsplit(url).netloc.split('.')
+    if netloc[-2] + netloc[-1] != website:
+        return
+
+    global url_list
+    if url in url_list:
+        return
+
+    try:
+        urlcontent = request.urlopen(url).read().decode('latin-1')
+        url_list.append(url)
+        log(url)
+    except:
+        return
+
+    soup = BeautifulSoup(''.join(urlcontent))
+    img_tags = soup.findAll('img')
+    for img_tag in img_tags:
+        img_url = img_tag['src']
+        if img_url.lower().endswith('.png'):
+            try:
+                resize_image(BytesIO(request.urlopen(parse.urljoin(url, img_url)).read()), img_url.split('/')[-1])
+            except:
+                pass
+
+    if level > 0:
+        link_tags = soup.findAll('a')
+        if len(link_tags) > 0:
+            for linkTag in link_tags:
+                try:
+                    link_url = linkTag['href']
+                    download_images(link_url, level - 1)
+                except:
+                    pass
+
+
+def resize_image(fp, name):
+    im = Image.open(fp)
+    if im.size != (436, 48):
+        return
+    im = crop(trim(im))
+    im.thumbnail([100, 14], Image.LANCZOS)
+    blob = BytesIO()
+    im.save(blob, 'png')
+    blob_service.put_block_blob_from_bytes('images', name, blob.getvalue(), x_ms_blob_content_type='image/png')
+    log('resized ' + file[:-4:])
+
+
+def replace_all(text, dic):
+    for i, j in dic.items():
+        text = text.replace(i, j)
+    return text
+
+
+def trim(im):
+    return im.crop(im.getbbox())
+
+
+def crop(im):
+    return im.crop([357, 5, im.size[0] - 6, im.size[1] - 4])
+
+
+if os.path.isfile(os.path.join(os.path.dirname(__file__), 'settings.cfg')):
+	config = ConfigParser()
+	config.read(os.path.join(os.path.dirname(__file__), 'settings.cfg'))
+	
+	subreddit = config.get('reddit', 'subreddit')
+	username = config.get('reddit', 'username')
+	password = config.get('reddit', 'password')
+	
+	storage_account_name = config.get('azure', 'name')
+	storage_account_key = config.get('azure', 'key')
+else:
+    subreddit = os.getenv('SUBREDDIT')
+    username = os.getenv('USERNAME')
+    password = os.getenv('PASSWORD')
+
+    storage_account_name = os.getenv('STORAGE_ACCOUNT_NAME')
+    storage_account_key = os.getenv('STORAGE_ACCOUNT_KEY')
+
+storage_account = CloudStorageAccount(storage_account_name, storage_account_key)
+
+table_service = storage_account.create_table_service()
+blob_service = storage_account.create_blob_service()
+
+blob_service.create_container('images', x_ms_blob_public_access='container')
+table_service.create_table('logs')
+
+rooturl = 'http://www.oarspotter.com'
+netloc = parse.urlsplit(rooturl).netloc.split('.')
+website = netloc[-2] + netloc[-1]
+
+while True:
+    url_list = []
+    log('scraping oarspotter')
+    download_images(rooturl, 2)
+    log('finished scraping oarspotter')
+
+    replacements = {'_': ' ',
+                    '-': ' ',
+                    'Uni': 'University'}
+
+    log('Generating dictionary')
+    flair, content = {}, 'Howto\n==\nClick the link for your club, edit the "YourTextHere" part to whatever you want, but leave everything else including the quotation marks. Submit the message form and wait for max. 5 minutes. If it doesn\'t work, message the mods.\n\n'
+
+    for file in blob_service.list_blobs('images'):
+        value = replace_all(file.name[:-4:], replacements)
+        value = re.sub(r'((?<=[a-z])[A-Z]|(?<!\A)[A-Z](?=[a-z]))', r' \1', value)
+        content += '* [' + value + '](//reddit.com/message/compose/?to=RowingFlairBot&subject=f&message={"' + file.name[:-4:] + '":"YourTextHere"})\n'
+
+    r = praw.Reddit('RowingFlair by /u/Jammie1')
+    r.login(username, password)
+
+    r.edit_wiki_page(subreddit, 'flair', content, '')
+    log('Done!')
+    time.sleep(86400)
